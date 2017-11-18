398f21bf629e4831cf17b92e90616ad902588cbb
Prayag <dedaniyaprayag@gmail.com>
2017-11-19 00:04:00 +0530

    initial Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMsg());
+        }
+
+        public static string createMsg()
+        {
+            return "Hello World";
+        }
+    }
+}
